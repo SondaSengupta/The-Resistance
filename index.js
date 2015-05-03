29bc696ed74292d440d0a3b9fbbd0654be5f0012
Colby DeHart <colbydehart@gmail.com>
2015-05-03 11:00:37 -0500

    mongolaaab

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+/*jshint node: true*/
 var express = require('express'),
-    app = express(),
-    mongoose = require('mongoose'),
-    bodyParser = require('body-parser'),
-    server = require('http').createServer(app),
-    io = require('socket.io')(server);
+    app = express();
 
 app.set('port', (process.env.PORT || 5000));
+
+var server = require('http').createServer(app),
+    io = require('socket.io')(server),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
+
 app.set('view engine', 'jade');
 app.set('views', './views');
 
-mongoose.connect('localhost:27017/theResistance1');
+mongoose.connect('mongodb://cdehart:arstarst@ds031892.mongolab.com:31892/the-resistance');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,6 +22,4 @@ require('./sockets')(io);
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+server.listen(app.get('port'));
