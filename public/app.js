e2d1797e1c4375ca64519e8e6d6888e3ed4c06e4
Colby DeHart <colbydehart@gmail.com>
2015-05-02 21:36:47 -0500

    v 1.0.0

--- public/app.js
+++ public/app.js
@@ -10,18 +10,32 @@ $(function(){
 });
 
 function run(game){
+    var roleTemp, voteTemp, teamTemp;
     var $game = $('#game');
     var socket = io();
     var name;
-    //Join the game
-    $('#join').click(function(){
-        name = $('#name').val();
-        socket.emit('join', {
-            name: name,
-            game: game
+    $.get('/vote.html')
+    .then(function(data){
+        voteTemp = _.template(data);
+        return $.get('/team.html');
+    })
+    .then(function(data){
+        teamTemp = _.template(data);
+        return $.get('/roles.html');
+    })
+    .then(function(data){
+        roleTemp = _.template(data);
+        //Join the game
+        $('#join').click(function(){
+            name = $('#name').val();
+            socket.emit('join', {
+                name: name,
+                game: game
+            });
+            $game.html($('<p>').text('waiting for players to join...'));
         });
-        $game.html($('<p>').text('waiting for players to join...'));
     });
+
 
     socket.on('start_game', turn);
 
@@ -30,21 +44,80 @@ function run(game){
         $game.html('');
     }
 
-    var templ = _.template($('#roles').html());
     socket.on('roles', function(data){
         if(data.player.spy){
             toastr.warning('YOU ARE A SPY');
         }
-        $game.html(templ(data));
+        $game.html(roleTemp(data));
+        $('#selectTeam').click(function(){
+            var $checked = $('.teamNames:checked');
+            if($checked.length !== data.mission.required){
+                toastr.warning('You must select ' +
+                    data.mission.required + 
+                    ' candidates for this mission.');
+                return false;
+            }
+            var team = [];
+            $checked.each(function(){
+                team.push($(this).val());
+            });
+            socket.emit('propose_team', {team:team, id: game});
+        });
     });
 
-    //Team leader should submit by emitting a 'propose_team'
+    socket.on('team_vote_request', function(data){
+        $game.html(voteTemp(data));
+        $('#vote').click(function(){
+            choice = Boolean($('#vote-val').val());
+            socket.emit('team_vote', {
+                choice: choice,
+                id: game,
+                team: data
+            });
+            $game.html('');
+        });
+    });
 
-    function leaderDisplay(data){
-    }
+    socket.on('teamSuccess', function(data){
+        console.log(data);
+        if(data.team.indexOf(name) !== -1)
+            data.chosen = true;
+        else
+            data.chosen = false;
+        $game.html(teamTemp(data));
+        $('#vote').click(function(){
+            choice = Boolean($('#vote-val').val());
+            socket.emit('mission_vote', {
+                choice: choice,
+                id: game,
+                mission: data.mission
+            });
+            $game.html('');
+        });
+    });
 
-    function regularDisplay(data){
-    }
+    socket.on('next_turn', function(data){
+        toastr.info('The mission was a ' + data.result);
+        turn();
+    });
+
+    socket.on('next_turn_team', function(data){
+        toastr.info('The mission was voted down. ' + 
+                    data.success + ' yays to ' +
+                    data.failure + ' nays.');
+        turn();
+    });
+
+    socket.on('game_over', function(data){
+        var msg;
+        if(data.successes > data.failures){
+            msg = 'YOU WON! YOU KILLED THE GOVERNMENT'
+        }
+        else{
+            msg = 'YOU LOST. THE GOVERNMENT KILLED YOU!'
+        }
+        $game.html($('<h1>').text(msg));
+    });
 
     socket.on('message', function(msg){
         toastr.info(msg);
--- sockets.js
+++ sockets.js
@@ -43,6 +43,12 @@ module.exports = function(io){
         function startGame(id){
             Game.findById(id)
             .then(function(found){
+                var game = createGame(found.count);
+                found.missions = game.missions;
+                found.vote = game.vote;
+                return found.save();
+            })
+            .then(function(found){
                 found.started = true;
                 rand = _.shuffle(_.range(found.count));
                 for (var i = 0; i < found.spies; i++) {
@@ -52,6 +58,8 @@ module.exports = function(io){
                     found.players[rand[i]].spy = false;
                 };
                 found.markModified('players');
+                found.vote = [];
+                found.markModified('vote');
                 found.save(function(err, data){
                     io.in(id).emit('start_game');
                 });
@@ -65,7 +73,8 @@ module.exports = function(io){
                 var player = game.players[i];
                 var names = _.pluck(game.players, 'name');
                 var leader = i === game.leader;
-                var mission = _.first(game.missions, {completed: false});
+                var mission = getFirstMission(game.missions);
+                console.log('roles mission', mission);
                 socket.emit('roles', {
                     names: names,
                     player: player,
@@ -80,7 +89,8 @@ module.exports = function(io){
         id: id of game
         */
         socket.on('propose_team', function(data){
-            io.in(data.id).emit('team_vote_request', data.team);
+            io.in(data.id).emit('message', 'what about this team? ' + data.team);
+            io.in(data.id).emit('team_vote_request', {vote: data.team});
         });
 
         /*
@@ -89,7 +99,7 @@ module.exports = function(io){
         team: array of team names
         */
         socket.on('team_vote', function(vote){
-            Game.findById(id)
+            Game.findById(vote.id)
             .then(function(data){
                 data.vote.push(vote.choice);
                 data.markModified(vote);
@@ -97,44 +107,130 @@ module.exports = function(io){
             })
             .then(function(data){
                 if(data.vote.length === data.count)
-                    tallyVotes(data, 'team', vote.team);
+                    tallyVotes(vote.id, 'team', vote.team);
+            });
+        });
+
+        socket.on('mission_vote', function(data){
+            Game.findById(data.id)
+            .then(function(g){
+                g.vote.push(data.choice);
+                g.markModified(data);
+                return g.save();
+            })
+            .then(function(g){
+                console.log('voting', g.vote.length, data.mission);
+                if(g.vote.length === data.mission.required)
+                    tallyVotes(data.id, 'mission', data.mission);
             });
         });
 
         /*
-        data: list of team members or ...
         game: id of game
         type: string 'team' or 'mission'
+        data: list of team members or mission object.
         */
         function tallyVotes (game, type, data) {
+            if(type==='mission'){
+                var result;
+                Game.findById(game)
+                .then(function(g){
+                    var mission = getFirstMission(g.missions);
+                    console.log('first mission (mission)', mission);
+                    var nays = g.vote.reduce(function(sum, val){
+                        console.log('sum', sum, 'val', val);
+                        var x = val === true ? 0 : 1;
+                        return sum + x;
+                    }, 0);
+                    console.log('nays', nays, 'vote', g.vote);
+                    if(nays === 0 || (mission.double && nays < 2)){
+                        result = 'success';
+                        g.vote = [];
+                        g.markModified('vote');
+                        mission.completed = 'success';
+                        g.markModified('missions');
+                    }
+                    else{
+                        result = 'failure';
+                        g.vote = [];
+                        g.markModified('vote');
+                        mission.completed = 'failure';
+                        g.markModified('missions');
+                    }
+                    return g.save();
+                })
+                .then(function(g){
+                    var successes, failures;
+                    successes = failures = 0;
+                    g.missions.forEach(function(val){
+                        if(val.completed === 'success')
+                            successes++;
+                        if(val.completed === 'failure')
+                            failures++;
+                    });
+                    console.log('fail', failures, 'success', successes);
+                    if(failures > 2 || successes > 2){
+                        io.in(game).emit('game_over', {successes: successes, failures: failures});
+                    }
+                    else{
+                        g.leader = (g.leader+1)%g.count;
+                        g.save()
+                        .then(function(g){
+                            io.in(game).emit('next_turn', {result: result});
+                        });
+                    }
+                });
+            }
             if(type==='team'){
                 Game.findById(game)
                 .then(function(g){
-                    var mission = _.first(g.missions, {completed: false});
-                    var yays = data.reduce(function(sum, val){return sum + Number(val);});
-                    if (yays > data.length/2 ){
-                        io.in(game).emit('teamSuccess',{
-                            team: data,
-                            mission: mission
+                    var mission = getFirstMission(g.missions);
+                    console.log('first mission (team)', mission);
+                    var yays = g.vote.reduce(function(sum, val){return sum + Number(val);});
+                    if (yays > g.count/2 ){
+                        g.vote = [];
+                        g.markModified('vote');
+                        g.save()
+                        .then(function(){
+                            io.in(game).emit('teamSuccess',{
+                                team: data.vote,
+                                mission: mission,
+                                yays: yays,
+                                nays: data.length - yays
+                            });
                         });
                     }
                     else
-                        failTeam(game);
+                        failTeam(game, yays);
                 });
+
+                function failTeam(id, yays){
+                    Game.findById(id)
+                    .then(function(g){
+                        g.vote = [];
+                        g.markModified('vote');
+                        g.leader = (g.leader+1)%g.count;
+                        return g.save();
+                    })
+                    .then(function(g){
+                        io.in(id).emit('next_turn_team', {success: yays, failure: g.count - yays});
+                    });
+                }
             }
         }
+
 
 
         socket.on('disconnect', function(){
             if(name){
                 Game.findById(room)
                 .then(function(data){
-                    game = data;
+                    var game = data;
                     game.started = false;
                     game.players = game.players.filter(function(el){return el.name !== name});
                     return game.save();
                 })
-                .then(function(data){
+                .then(function(game){
                     io.to(room).emit('message', name + ' has disconnected');
                     // if(!game.players.length || game.started){
                     //     endGame(room);
@@ -153,3 +249,42 @@ module.exports = function(io){
     //End 'on connection'
     });
 };
+
+function createGame(len){
+    var Game = require('./models/game');
+    if (len<5||len>10){ return null; }
+    var numOfMissions = [
+        '2,3,2,3,3',
+        '2,3,3,3,4',
+        '2,3,3,4*,4',
+        '3,4,4,5*,5',
+        '3,4,4,5*,5',
+        '3,4,4,5*,5'
+    ];
+    var missions = numOfMissions[len-5].split(',');
+    var createMission = function(num){
+        return {
+            required: Number(num[0]),
+            double: num.indexOf('*') !== -1,
+            completed: false
+        };
+    };
+    var spies = '2,2,3,3,3,4'.split(',');
+    missions = missions.map(createMission);
+    return new Game({
+        spies: Number(spies[len-5]),
+        count: len,
+        players: [],
+        missions: missions,
+        leader: 0,
+        started: false,
+        vote: [],
+    });
+}
+
+function getFirstMission(missions){
+    for (var i = 0; i < missions.length; i++) {
+        if (missions[i].completed === false)
+            return missions[i];
+    }
+}
